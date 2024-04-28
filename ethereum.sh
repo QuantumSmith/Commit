ccd768be9321f58f70ea9228355215cd77fe46c9
Quantum Smith <163209788+QuantumSmith@users.noreply.github.com>
2024-04-28 22:49:19 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-26bb5a7e-a410-46fe-a177-7ba0ad85c68b/"
 
 OUTPUT_DIR="logs"
 
